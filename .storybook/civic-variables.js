4d189484330cb0d6b1c35aa22660def509e9bb0a
Deployer Robot <deployer@salsadigital.com.au>
2021-09-30 01:19:08 +1000

    Release 0.5.0.
    Dsisabled debug.
    Added creation of user accounts and fixed site email. (#3)

--- .storybook/civic-variables.js
+++ .storybook/civic-variables.js
@@ -1,47 +1,54 @@
-// Extracts an object with all mapping variables from components/variables.scss.
+//
+// Extract CSS variables as object from SCSS file.
+//
 const fs = require('fs');
 
-const importedFile = fs.readFileSync('../components/variables.scss', { encoding:'utf8' })
-
-function getSassVariableGroups () {
-  const reg = new RegExp('\\$[a-z-]+:\\s\\(([\\s\\S\\r])+?\\;', 'gi')
-  const groups = []
-
-  // Match
-  let lastMatch = reg.exec(importedFile)
-  while (lastMatch !== null) {
-    groups.push(lastMatch[0])
-    lastMatch = reg.exec(importedFile)
-  }
-  return groups
-}
-
-function getSassVariablesFromGroup (group) {
-  const reg = new RegExp('\'[a-z-_0-9]+\'', 'gi')
-  const returnVars = []
-
-  // Match
-  let match = reg.exec(group)
-  while (match !== null) {
-    returnVars.push(match[0].replace(/[\':]/gi, ''))
-    match = reg.exec(group)
-  }
-  return returnVars
-}
+const importedFile = fs.readFileSync('./components/00-base/_variables.scss', { encoding:'utf8' })
 
 function getGroups () {
   const allGroups = {}
+
   const groups = getSassVariableGroups()
+
   groups.forEach(group => {
-    const r = new RegExp('(\\$[a-z-]+\\:)', 'gim')
-    let match = r.exec(group)
+    const re = new RegExp('(\\$[a-z-]+\\:)', 'gim')
+    let match = re.exec(group)
     if (match) {
       let name = match[0].replace(/[\$\:]/gi, '')
       const strippedGroup = group.replace(/\$[a-z-]+\:/gi, '').replace(/\(.+?\)/gi, '').replace(/:\s\([\s\S\n\r]+?\)/gi, '')
       allGroups[name] = getSassVariablesFromGroup(strippedGroup)
     }
   })
+
   return allGroups
+}
+
+function getSassVariableGroups () {
+  const groups = []
+
+  const re = new RegExp('\\$[a-z-]+:\\s\\(([\\s\\S\\r])+?\\;', 'gi')
+
+  let lastMatch = re.exec(importedFile)
+  while (lastMatch !== null) {
+    groups.push(lastMatch[0])
+    lastMatch = re.exec(importedFile)
+  }
+
+  return groups
+}
+
+function getSassVariablesFromGroup (group) {
+  const vars = []
+
+  const re = new RegExp('\'[a-z-_0-9]+\'', 'gi')
+
+  let match = re.exec(group)
+  while (match !== null) {
+    vars.push(match[0].replace(/[\':]/gi, ''))
+    match = re.exec(group)
+  }
+
+  return vars
 }
 
 module.exports = {
