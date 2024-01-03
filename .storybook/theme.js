13d053d397d8d0c595b9134340aece8e4f7c5856
Alex Skrypnyk <alex@drevops.com>
2024-01-03 14:29:29 +1100

    Updated references.

--- .storybook/theme.js
+++ .storybook/theme.js
@@ -6,6 +6,6 @@ import { create } from '@storybook/theming';
 export default create({
   base: 'light',
   brandTitle: 'CivicTheme Library',
-  brandUrl: 'https://github.com/salsadigitalauorg/civictheme_library',
+  brandUrl: 'https://github.com/civictheme/uikit',
   brandImage: './assets/logos/logo_secondary_light_desktop.png',
 });
