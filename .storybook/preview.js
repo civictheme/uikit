0129c1fcea5236456eca0804619ee1c793b7f90d
Joshua Fernandes <joshua.1234511@yahoo.in>
2024-01-25 10:11:16 +0530

    [83] Updated storybook to show HTML source.

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -94,7 +94,7 @@ export const parameters = {
     prettier: {
       tabWidth: 4,
       useTabs: false,
-      htmlWhitespaceSensitivity: "strict",
+      htmlWhitespaceSensitivity: 'strict',
     },
   },
 };
