ee51bf89f79103fe25a848fcca57be40de6570a2
Joshua Fernandes <83997348+joshua-salsadigital@users.noreply.github.com>
2024-03-01 11:01:07 +0530

    #90 Updated content in table cells to be top aligned. (#91)
    Co-authored-by: Joshua Fernandes <“joshua.1234511@yahoo.in”>

--- components/01-atoms/table/table.stories.js
+++ components/01-atoms/table/table.stories.js
@@ -55,6 +55,18 @@ export const Table = (knobTab) => {
         'Another column',
         'One more column column',
       ],
+      [
+        'Row 6 with larger amount of content',
+        '<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p><p>Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p><p>It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</p><p>It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>',
+        'Another column',
+        'One more column column',
+      ],
+      [
+        'Row 7 with small amount of content',
+        '<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>',
+        'Another column',
+        'One more column column',
+      ],
     ];
   };
 
