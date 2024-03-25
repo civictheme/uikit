d091fe11b6f1ba82a661e7c5b2d986ff0cdd8e5b
Joshua Fernandes <joshua.1234511@yahoo.in>
2024-03-25 14:47:37 +0530

    Added script to the Single and Group Filter components.

--- components/02-molecules/group-filter/group-filter.js
+++ components/02-molecules/group-filter/group-filter.js
@@ -3,6 +3,16 @@
  * Group Filter component.
  */
 function CivicThemeGroupFilterComponent(el) {
+  if (this.el) {
+    return;
+  }
+
+  this.el = el;
+
+  this.el.addEventListener('ct.group-filter.update', () => {
+    this.update(true);
+  });
+
   // Function to trigger custom event
   const triggerFilterUpdateEvent = function () {
     el.dispatchEvent(new CustomEvent('ct.group-filter.update'));
@@ -11,7 +21,7 @@ function CivicThemeGroupFilterComponent(el) {
   // Function to bind event listeners
   const bindEventListeners = function () {
     // Add event listener for filter change
-    el.querySelectorAll('input').forEach((input) => {
+    el.querySelectorAll('input, textarea, select, [type="checkbox"], [type="radio"]').forEach((input) => {
       input.addEventListener('change', triggerFilterUpdateEvent);
     });
   };
@@ -24,10 +34,11 @@ function CivicThemeGroupFilterComponent(el) {
  * Update the instance.
  */
 CivicThemeGroupFilterComponent.prototype.update = function () {
-  // TODO
+  const t = this;
+  t.el.setAttribute('aria-live', 'polite');
 };
 
 // Find group Filter components and initialize
-document.querySelectorAll('[data-component-name="ct-group-filter"]').forEach((el) => {
+document.querySelectorAll('[data-ct-group-filter-filters]').forEach((el) => {
   new CivicThemeGroupFilterComponent(el);
 });
