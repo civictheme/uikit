a7f313e4b48a247f9137f252d20fac4b9ee264ea
Alex Skrypnyk <alex@drevops.com>
2024-06-04 20:08:18 +1000

    Added support for contained.

--- components/00-base/layout/layout.stories.js
+++ components/00-base/layout/layout.stories.js
@@ -11,10 +11,11 @@ export default {
 export const Layout = (props = {}) => {
   const knobs = {
     rail_top_left: knobBoolean('Show top left rail', true, props.rail_top_left, props.knobTab) ? placeholder('Top left rail') : '',
-    rail_top_right: knobBoolean('Show top right rail', true, props.rail_top_right, props.knobTab) ? placeholder('Top right rail') : '',
-    content: knobBoolean('Show content', true, props.content, props.knobTab) ? placeholder('Content') : '',
     rail_bottom_left: knobBoolean('Show bottom left rail', true, props.rail_bottom_left, props.knobTab) ? placeholder('Bottom left rail') : '',
+    content: knobBoolean('Show content', true, props.content, props.knobTab) ? placeholder('Content') : '',
+    rail_top_right: knobBoolean('Show top right rail', true, props.rail_top_right, props.knobTab) ? placeholder('Top right rail') : '',
     rail_bottom_right: knobBoolean('Show bottom right rail', true, props.rail_bottom_right, props.knobTab) ? placeholder('Bottom right rail') : '',
+    is_contained: knobBoolean('Is contained', false, props.is_contained, props.knobTab),
     vertical_spacing: knobRadios(
       'Vertical spacing',
       {
