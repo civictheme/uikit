3ecf57e2309cefc2b5dd40a1e538103aee7e4f63
Alex Skrypnyk <alex@drevops.com>
2024-06-12 17:15:53 +1000

    Updated checkbox to use SVG mask.

--- components/01-atoms/checkbox/checkbox.stories.js
+++ components/01-atoms/checkbox/checkbox.stories.js
@@ -1,5 +1,5 @@
 import CivicThemeCheckbox from './checkbox.twig';
-import { knobText, knobBoolean, knobRadios, shouldRender } from '../../00-base/base.utils';
+import { knobBoolean, knobRadios, knobText, randomId, randomInt, randomName, shouldRender } from '../../00-base/base.utils';
 
 export default {
   title: 'Atoms/Checkbox',
@@ -19,13 +19,15 @@ export const Checkbox = (parentKnobs = {}) => {
       'light',
       parentKnobs.knobTab,
     ),
-    content: knobText('Content', 'Checkbox label', parentKnobs.knobTab),
+    label: knobText('Label', 'Checkbox label', parentKnobs.knobTab),
+    name: randomName(),
+    id: randomId(),
+    value: randomInt(1, 1000),
     is_required: knobBoolean('Required', false, parentKnobs.knobTab),
-    for: knobText('For', '', parentKnobs.knobTab),
-    disabled: knobBoolean('Disabled', false, parentKnobs.knobTab),
-    has_error: knobBoolean('Has error', false, parentKnobs.knobTab),
-    modifier_class: knobText('Additional classes', '', parentKnobs.knobTab),
+    is_disabled: knobBoolean('Disabled', false, parentKnobs.knobTab),
+    is_invalid: knobBoolean('Has error', false, parentKnobs.knobTab),
     attributes: knobText('Additional attributes', '', parentKnobs.knobTab),
+    modifier_class: knobText('Additional classes', '', parentKnobs.knobTab),
   };
 
   return shouldRender(parentKnobs) ? CivicThemeCheckbox(knobs) : knobs;
