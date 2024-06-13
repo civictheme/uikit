681ae50784058b38777a32d1d5926185c1775def
Alex Skrypnyk <alex@drevops.com>
2024-06-13 18:24:13 +1000

    Updated `Field` to corectly handle `Radios` and `Checkboxes`.
    Also, removed checkbox-group and radion group.
    
    Added support for horizontal Field layout.

--- components/01-atoms/textarea/textarea.stories.js
+++ components/01-atoms/textarea/textarea.stories.js
@@ -1,5 +1,5 @@
 import CivicThemeTextarea from './textarea.twig';
-import { knobBoolean, knobNumber, knobRadios, knobText, randomName, shouldRender } from '../../00-base/base.utils';
+import { knobBoolean, knobNumber, knobRadios, knobText, randomId, randomName, shouldRender } from '../../00-base/base.utils';
 
 export default {
   title: 'Atoms/Form Controls/Textarea',
@@ -37,9 +37,10 @@ export const Textarea = (parentKnobs = {}) => {
     placeholder: knobText('Placeholder', 'Placeholder', parentKnobs.placeholder, parentKnobs.knobTab),
     value: knobText('Value', '', parentKnobs.value, parentKnobs.knobTab),
     name: randomName(),
+    id: randomId(),
     is_required: knobBoolean('Required', false, parentKnobs.is_required, parentKnobs.knobTab),
-    is_disabled: knobBoolean('Disabled', false, parentKnobs.is_disabled, parentKnobs.knobTab),
     is_invalid: knobBoolean('Has error', false, parentKnobs.is_invalid, parentKnobs.knobTab),
+    is_disabled: knobBoolean('Disabled', false, parentKnobs.is_disabled, parentKnobs.knobTab),
     modifier_class: knobText('Additional classes', '', parentKnobs.modifier_class, parentKnobs.knobTab),
     attributes: knobText('Additional attributes', '', parentKnobs.attributes, parentKnobs.knobTab),
   };
