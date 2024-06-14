3262587a0b49d8e30b5b94278150a98716e43789
Alex Skrypnyk <alex@drevops.com>
2024-06-14 13:29:15 +1000

    Added docs for `Typography` component.

--- components/00-base/typography/typography.stories.js
+++ components/00-base/typography/typography.stories.js
@@ -2,6 +2,11 @@ import CivicThemeTypography from './typography.stories.twig';
 
 export default {
   title: 'Base/Typography',
+  parameters: {
+    docs: 'Demonstration of typography rules defined in the design system.<br/>For fully styled content, see <code>Basic&nbsp;content</code> component.',
+    docsSize: 'medium',
+    docsPlacement: 'before',
+  },
 };
 
 export const Typography = () => CivicThemeTypography();
