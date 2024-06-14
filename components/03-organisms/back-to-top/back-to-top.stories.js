b8d578aefe6579a18d8b9507e8050e6b5b4e96c1
Alex Skrypnyk <alex@drevops.com>
2024-06-14 13:11:14 +1000

    Updated `Back to Top` story's docs.

--- components/03-organisms/back-to-top/back-to-top.stories.js
+++ components/03-organisms/back-to-top/back-to-top.stories.js
@@ -4,10 +4,14 @@ import { shouldRender } from '../../00-base/base.utils';
 export default {
   title: 'Organisms/Back To Top',
   parameters: {
-    layout: 'centered',
+    layout: 'fullscreen',
+    docs: 'Back To Top button appears when the bottom of the red rectangle reaches the top of the page.',
+    docsSize: 'medium',
+    docsPlacement: 'before',
+    wrapperHtmlBefore: '<a id="top"></a><div class="example-container"><div class="example-container__page-content example-ct-back-to-top"></div>',
   },
 };
 
 export const BackToTop = (parentKnobs = {}) => (shouldRender(parentKnobs)
-  ? `<a id="top"></a><div class="example-container"><div class="example-container__page-content example-ct-back-to-top"><div class="docs-container docs-container--large">Back To Top button appears when the bottom of the red rectangle reaches the top of the page.</div></div>${CivicThemeBackToTop()}</div>`
+  ? CivicThemeBackToTop()
   : '');
