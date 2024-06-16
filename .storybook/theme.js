3f5541b13f85f7386b5899afb971415fc54f8da3
Alex Skrypnyk <alex@drevops.com>
2024-06-17 09:18:56 +1000

    Fixed the title on SB.

--- .storybook/theme.js
+++ .storybook/theme.js
@@ -5,7 +5,7 @@ import { create } from '@storybook/theming';
 
 export default create({
   base: 'light',
-  brandTitle: 'CivicTheme Library',
+  brandTitle: 'CivicTheme UI Kit',
   brandUrl: 'https://github.com/civictheme/uikit',
   brandImage: './assets/logos/logo_secondary_light_desktop.png',
 });
