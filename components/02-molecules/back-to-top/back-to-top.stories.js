51604011177883bc726d9718ce98b19e31a3e57b
Alex Skrypnyk <alex@drevops.com>
2024-06-16 22:57:12 +1000

    Moved `Back-to-Top` to Molecules.

--- components/02-molecules/back-to-top/back-to-top.stories.js
+++ components/02-molecules/back-to-top/back-to-top.stories.js
@@ -2,7 +2,7 @@ import CivicThemeBackToTop from './back-to-top.twig';
 import { shouldRender } from '../../00-base/storybook/storybook.utils';
 
 export default {
-  title: 'Organisms/Back To Top',
+  title: 'Molecules/Back To Top',
   parameters: {
     layout: 'fullscreen',
     docs: 'Back To Top button appears when the bottom of the red rectangle reaches the top of the page.',
