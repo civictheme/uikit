912f8f1dd8c86c3228b6f659532028911674d5f7
Alex Skrypnyk <alex@drevops.com>
2024-06-16 19:33:02 +1000

    Refactor `Pagination` story and linked up to `Field` component.

--- components/02-molecules/pagination/pagination.stories.js
+++ components/02-molecules/pagination/pagination.stories.js
@@ -1,24 +1,34 @@
 import CivicThemePagination from './pagination.twig';
-import { knobBoolean, knobNumber, knobRadios, knobText, randomUrl, shouldRender } from '../../00-base/storybook/storybook.utils';
+import { knobBoolean, knobNumber, knobRadios, knobText, randomId, randomName, randomUrl, shouldRender } from '../../00-base/storybook/storybook.utils';
 
 export default {
   title: 'Molecules/Pagination',
+  parameters: {
+    layout: 'fullscreen',
+  },
 };
 
 export const Pagination = (parentKnobs = {}) => {
-  const theme = knobRadios(
-    'Theme',
-    {
-      Light: 'light',
-      Dark: 'dark',
-    },
-    'light',
-    parentKnobs.theme,
-    parentKnobs.knobTab,
-  );
-  const activeIsLink = knobBoolean('Active is a link', true, parentKnobs.active_is_link, parentKnobs.knobTab);
+  const knobs = {
+    theme: knobRadios(
+      'Theme',
+      {
+        Light: 'light',
+        Dark: 'dark',
+      },
+      'light',
+      parentKnobs.theme,
+      parentKnobs.knobTab,
+    ),
+    active_is_link: knobBoolean('Active is a link', true, parentKnobs.active_is_link, parentKnobs.knobTab),
+    use_ellipsis: knobBoolean('With ellipsis', false, parentKnobs.use_ellipsis, parentKnobs.knobTab),
+    with_items_per_page: knobBoolean('With items per page', true, parentKnobs.with_items_per_page, parentKnobs.knobTab),
+    heading_id: knobText('Heading ID', 'ct-pager-demo', parentKnobs.heading_id, parentKnobs.knobTab),
+    modifier_class: knobText('Additional classes', '', parentKnobs.modifier_class, parentKnobs.knobTab),
+    attributes: knobText('Additional attributes', '', parentKnobs.attributes, parentKnobs.knobTab),
+  };
 
-  const pageCount = knobNumber(
+  knobs.count_of_pages = knobNumber(
     'Count of pages',
     5,
     {
@@ -31,27 +41,27 @@ export const Pagination = (parentKnobs = {}) => {
     parentKnobs.knobTab,
   );
 
-  const current = knobNumber(
+  knobs.current_page = knobNumber(
     'Current page',
-    Math.max(1, Math.floor(pageCount / 2)),
+    Math.max(1, Math.floor(knobs.count_of_pages / 2)),
     {
       range: true,
       min: 1,
-      max: pageCount,
+      max: knobs.count_of_pages,
       step: 1,
     },
     parentKnobs.current_page,
     parentKnobs.knobTab,
   );
-  const useEllipsis = knobBoolean('With ellipsis', false, parentKnobs.use_ellipsis, parentKnobs.knobTab);
+
+  const pagerMiddle = Math.ceil(knobs.count_of_pages / 2);
+  const pagerFirst = knobs.current - pagerMiddle + 1;
+  const pagerLast = knobs.current + knobs.count_of_pages - pagerMiddle;
 
   const pages = {};
-  const pagerMiddle = Math.ceil(pageCount / 2);
-  const pagerFirst = current - pagerMiddle + 1;
-  const pagerLast = current + pageCount - pagerMiddle;
-  for (let i = 0; i < pageCount; i++) {
-    if (useEllipsis) {
-      if (i === 0 || (i > pagerFirst && i < pagerLast) || i === (pageCount - 1)) {
+  for (let i = 0; i < knobs.count_of_pages; i++) {
+    if (knobs.use_ellipsis) {
+      if (i === 0 || (i > pagerFirst && i < pagerLast) || i === (knobs.count_of_pages - 1)) {
         pages[i + 1] = {
           href: randomUrl(),
         };
@@ -63,7 +73,7 @@ export const Pagination = (parentKnobs = {}) => {
     }
   }
 
-  const items = pageCount > 0 ? {
+  const items = knobs.count_of_pages > 0 ? {
     previous: {
       href: randomUrl(),
     },
@@ -73,33 +83,50 @@ export const Pagination = (parentKnobs = {}) => {
     },
   } : null;
 
-  const itemsPerPage = [
-    {
-      type: 'option', label: 10, value: 10, selected: false,
-    },
-    {
-      type: 'option', label: 20, value: 20, selected: true,
-    },
-    {
-      type: 'option', label: 50, value: 50, selected: false,
-    },
-    {
-      type: 'option', label: 100, value: 100, selected: false,
-    },
-  ];
-
-  const knobs = {
-    theme,
-    active_is_link: activeIsLink,
+  const props = {
+    theme: knobs.theme,
+    active_is_link: knobs.active_is_link,
     items,
-    heading_id: knobText('Heading Id', 'ct-pager-demo', parentKnobs.heading_id, parentKnobs.knobTab),
-    use_ellipsis: useEllipsis,
-    items_per_page_options: knobBoolean('With items per page', true, parentKnobs.with_items_per_page, parentKnobs.knobTab) ? itemsPerPage : null,
-    total_pages: pageCount,
-    current,
-    modifier_class: knobText('Additional classes', '', parentKnobs.modifier_class, parentKnobs.knobTab),
-    attributes: knobText('Additional attributes', '', parentKnobs.attributes, parentKnobs.knobTab),
+    heading_id: knobs.heading_id,
+    use_ellipsis: knobs.use_ellipsis,
+    total_pages: knobs.count_of_pages,
+    current: knobs.current,
+    modifier_class: knobs.modifier_class,
+    attributes: knobs.attributes,
   };
 
-  return shouldRender(parentKnobs) ? CivicThemePagination(knobs) : knobs;
+  if (knobs.with_items_per_page) {
+    props.items_per_page_label = 'Items per page';
+    props.items_per_page_name = randomName();
+    props.items_per_page_id = randomId();
+
+    props.items_per_page_options = [
+      {
+        type: 'option',
+        label: 10,
+        value: 10,
+        is_selected: false,
+      },
+      {
+        type: 'option',
+        label: 20,
+        value: 20,
+        is_selected: true,
+      },
+      {
+        type: 'option',
+        label: 50,
+        value: 50,
+        is_selected: false,
+      },
+      {
+        type: 'option',
+        label: 100,
+        value: 100,
+        is_selected: false,
+      },
+    ];
+  }
+
+  return shouldRender(parentKnobs) ? CivicThemePagination(props) : knobs;
 };
