0916ed7a70de629918e71671e5f3e7496654dd62
Alex Skrypnyk <alex@drevops.com>
2024-06-18 21:43:58 +1000

    Added `hide_sidebar_left` and `hide_sidebar_right` props to `Layout` and fixed mobile spacing.

--- components/00-base/layout/layout.stories.js
+++ components/00-base/layout/layout.stories.js
@@ -14,9 +14,11 @@ export const Layout = (parentKnobs = {}) => {
   const knobs = {
     sidebar_top_left: knobBoolean('Show top left sidebar', true, parentKnobs.sidebar_top_left, parentKnobs.knobTab) ? placeholder('Top left sidebar', useLargePlaceholders ? 50 : 0) : '',
     sidebar_bottom_left: knobBoolean('Show bottom left sidebar', true, parentKnobs.sidebar_bottom_left, parentKnobs.knobTab) ? placeholder('Bottom left sidebar', useLargePlaceholders ? 50 : 0) : '',
+    hide_sidebar_left: knobBoolean('Hide left sidebar', false, parentKnobs.hide_sidebar_left, parentKnobs.knobTab),
     content: knobBoolean('Show content', true, parentKnobs.content, parentKnobs.knobTab) ? placeholder('Content', useLargePlaceholders ? 1000 : 0) : '',
     sidebar_top_right: knobBoolean('Show top right sidebar', true, parentKnobs.sidebar_top_right, parentKnobs.knobTab) ? placeholder('Top right sidebar', useLargePlaceholders ? 50 : 0) : '',
     sidebar_bottom_right: knobBoolean('Show bottom right sidebar', true, parentKnobs.sidebar_bottom_right, parentKnobs.knobTab) ? placeholder('Bottom right sidebar', useLargePlaceholders ? 50 : 0) : '',
+    hide_sidebar_right: knobBoolean('Hide right sidebar', false, parentKnobs.hide_sidebar_right, parentKnobs.knobTab),
     is_contained: knobBoolean('Is contained', false, parentKnobs.is_contained, parentKnobs.knobTab),
     vertical_spacing: knobRadios(
       'Vertical spacing',
@@ -32,6 +34,18 @@ export const Layout = (parentKnobs = {}) => {
     ),
   };
 
+  const showNested = knobBoolean('Show nested layout', false, parentKnobs.show_nested_layout, parentKnobs.knobTab);
+
+  knobs.content = showNested ? CivicThemeLayout({
+    ...knobs,
+    ...slotKnobs([
+      'content_top',
+      'content_bottom',
+    ]),
+  }) : knobs.content;
+
+  const showOutlines = knobBoolean('Show outlines', false, parentKnobs.show_outlines, parentKnobs.knobTab);
+
   const attributesTab = 'Attributes';
   knobs.sidebar_top_left_attributes = knobs.sidebar_top_left ? knobText('Top left sidebar attributes', '', parentKnobs.sidebar_top_left_attributes, attributesTab) : '';
   knobs.sidebar_bottom_left_attributes = knobs.sidebar_bottom_left ? knobText('Bottom left sidebar attributes', '', parentKnobs.sidebar_bottom_left_attributes, attributesTab) : '';
@@ -41,12 +55,12 @@ export const Layout = (parentKnobs = {}) => {
   knobs.modifier_class = knobText('Additional class', '', parentKnobs.modifier_class, parentKnobs.knobTab);
 
   return shouldRender(parentKnobs)
-    ? CivicThemeLayout({
+    ? `<div class="${showOutlines ? 'story-layout-outlines' : ''}">${CivicThemeLayout({
       ...knobs,
       ...slotKnobs([
         'content_top',
         'content_bottom',
       ]),
-    })
+    })}</div>`
     : knobs;
 };
