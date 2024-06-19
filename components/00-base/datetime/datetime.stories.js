acb51f977616aefd29455aa98352429b4b9334a3
Alex Skrypnyk <alex@drevops.com>
2024-06-19 10:58:17 +1000

    Renamed `time` component to `datetime`.

--- components/00-base/datetime/datetime.stories.js
+++ components/00-base/datetime/datetime.stories.js
@@ -1,14 +1,14 @@
-import CivicThemeTime from './time.twig';
+import CivicThemeTimestamp from './datetime.twig';
 import { dateIsValid, knobText, shouldRender } from '../storybook/storybook.utils';
 
 export default {
-  title: 'Base/Utilities/Time',
+  title: 'Base/Utilities/Datetime',
   parameters: {
     layout: 'centered',
   },
 };
 
-export const Time = (parentKnobs = {}) => {
+export const Datetime = (parentKnobs = {}) => {
   const knobs = {
     start: knobText('Start', '20 Jan 2023 11:00', parentKnobs.start, parentKnobs.knobTab),
     end: knobText('End', '21 Jan 2023 15:00', parentKnobs.end, parentKnobs.knobTab),
@@ -19,5 +19,5 @@ export const Time = (parentKnobs = {}) => {
   knobs.start_iso = dateIsValid(knobs.start) ? new Date(knobs.start).toISOString() : null;
   knobs.end_iso = dateIsValid(knobs.end) ? new Date(knobs.end).toISOString() : null;
 
-  return shouldRender(parentKnobs) ? CivicThemeTime(knobs) : knobs;
+  return shouldRender(parentKnobs) ? CivicThemeTimestamp(knobs) : knobs;
 };
