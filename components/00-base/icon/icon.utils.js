c6e993c45553c88855bdce04b9a48b2ed4afd370
Alan Cole <alan.cole@salsa.digital>
2024-11-28 10:45:24 +1100

    [CIVIC-1947] Added Storybook v8, vite, new build.

--- components/00-base/icon/icon.utils.js
+++ components/00-base/icon/icon.utils.js
@@ -3,10 +3,10 @@
  * Icon component utilities.
  */
 
-const fs = require('fs');
-const pathUtil = require('path');
+import fs from 'fs';
+import pathUtil from 'path';
 
-const basePath = pathUtil.resolve(__dirname, '../../../assets/icons');
+const basePath = pathUtil.resolve(import.meta.dirname, '../../../assets/icons');
 
 function getIconPaths() {
   const iconPaths = [];
@@ -30,6 +30,6 @@ function getIcons() {
   return getIconPaths().map((path) => getIconNameFromPath(path));
 }
 
-module.exports = {
-  getIcons,
-};
+export default {
+  getIcons
+}
