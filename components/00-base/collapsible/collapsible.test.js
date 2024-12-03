fbd4888ce2657e6e70661dc3181cea5d0d2f9052
Alan Cole <alan.cole@salsa.digital>
2024-12-03 11:52:33 +1100

    [#425] Improve tests.

--- components/00-base/collapsible/collapsible.test.js
+++ components/00-base/collapsible/collapsible.test.js
@@ -1,12 +1,45 @@
 describe('Collapsible utility', () => {
-
-  beforeEach(() => {
+  beforeAll(() => {
     // jsdom doesn't support innerText by default.
     // https://github.com/jsdom/jsdom/issues/1245
     Object.defineProperty(HTMLDivElement.prototype, 'innerText', {
-      get: function() { return this.textContent; },
-      set: function(value) { this.textContent = value; }
+      get() {
+        return this.textContent;
+      },
+      set(value) {
+        this.textContent = value;
+      },
     });
+  });
+
+  beforeEach(() => {
+    jest.resetModules();
+  });
+
+  test('Clicking trigger when closed expands the panel', async () => {
+    document.body.innerHTML = `
+      <div data-collapsible data-collapsible-collapsed data-collapsible-duration="0">
+        <div data-collapsible-trigger>
+          My trigger
+        </div>
+        <div data-collapsible-panel>
+          My panel
+        </div>
+      </div>
+    `;
+
+    // eslint-disable-next-line global-require
+    require('./collapsible');
+
+    document.querySelector('[data-collapsible-trigger]').click();
+
+    expect(document.querySelector('[data-collapsible-trigger]').getAttribute('aria-expanded')).toEqual('true');
+    expect(document.querySelector('[data-collapsible-panel]').getAttribute('aria-hidden')).toEqual('false');
+
+    document.querySelector('[data-collapsible-trigger]').click();
+
+    expect(document.querySelector('[data-collapsible-trigger]').getAttribute('aria-expanded')).toEqual('false');
+    expect(document.querySelector('[data-collapsible-panel]').getAttribute('aria-hidden')).toEqual('true');
   });
 
   test('Renders icons when using icon group', async () => {
@@ -21,7 +54,8 @@ describe('Collapsible utility', () => {
       </div>
     `;
 
-    require('./collapsible.js');
+    // eslint-disable-next-line global-require
+    require('./collapsible');
 
     expect(document.querySelectorAll('[data-collapsible-trigger] .ct-text-icon__group')).toHaveLength(1);
     expect(document.querySelector('[data-collapsible-trigger] .ct-text-icon__group').textContent.trim()).toEqual('trigger');
