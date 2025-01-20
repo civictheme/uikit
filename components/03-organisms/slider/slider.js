2ec7161f0d7888040418a7f8bded07b8c56bb1dc
Feb Dao <dinhhien102@gmail.com>
2025-01-20 14:36:42 +1100

    [#294] Slider issue (#432)
    Co-authored-by: Joshua Fernandes <“joshua.1234511@yahoo.in”>

--- components/03-organisms/slider/slider.js
+++ components/03-organisms/slider/slider.js
@@ -34,19 +34,23 @@ function CivicThemeSlider(el) {
 CivicThemeSlider.prototype.refresh = function () {
   // Set slide width based on panel width.
   const panelWidth = window.getComputedStyle(this.panel).width;
-  this.slides.forEach((slide) => {
-    slide.style.width = panelWidth;
-  });
+  const panelWidthVal = parseFloat(panelWidth);
+
+  // Reset rail and panel height.
+  this.rail.style.height = '';
+  this.panel.style.height = '';
 
   // Set the rail width.
-  const panelWidthVal = parseFloat(panelWidth);
   this.rail.style.width = `${this.totalSlides * panelWidthVal}px`;
 
   // Reset slide heights.
   this.slides.forEach((slide) => {
     slide.style.height = null;
+    slide.style.width = panelWidth;
   });
-  this.showAllSlides();
+
+  // Show all slides temporarily to calculate heights.
+  this.slides.forEach((slide) => slide.removeAttribute('data-slider-slide-hidden'));
 
   // Set slide position and find largest slide.
   let largestHeight = 0;
@@ -93,6 +97,8 @@ CivicThemeSlider.prototype.showAllSlides = function () {
 CivicThemeSlider.prototype.hideAllSlidesExceptCurrent = function () {
   this.slides.forEach((slide, idx) => {
     if (idx !== this.currentSlide) {
+      slide.setAttribute('data-slider-slide-hidden', 'true');
+    } else {
       slide.removeAttribute('data-slider-slide-hidden');
       slide.setAttribute('inert', true);
     }
@@ -103,7 +109,7 @@ CivicThemeSlider.prototype.updateDisplaySlide = function () {
   const duration = parseFloat(window.getComputedStyle(this.rail).transitionDuration) * 1000;
 
   this.disableSlideInteraction();
-  this.showAllSlides();
+  this.slides.forEach((slide) => slide.removeAttribute('data-slider-slide-hidden'));
 
   // Reset timer and wait for animation to complete.
   clearTimeout(this.animationTimeout);
