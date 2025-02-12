ad583395d3336d20f7d59e4148af0d3b14f94df3
richardgaunt <57734756+richardgaunt@users.noreply.github.com>
2025-02-13 09:23:55 +1100

    [CIVIC-1947] Added error handling to keep watch working. (#474)
    Co-authored-by: Alan Cole <alan.cole@salsa.digital>

--- build.js
+++ build.js
@@ -150,7 +150,7 @@ function buildCombineDirectories() {
     runCommand(`rsync -a --delete ${DIR_UIKIT_COMPONENTS_IN}/ ${DIR_UIKIT_COPY_OUT}/`)
     runCommand(`rsync -a --delete ${DIR_UIKIT_COPY_OUT}/ ${DIR_COMPONENTS_OUT}/`)
     runCommand(`rsync -a ${DIR_COMPONENTS_IN}/ ${DIR_COMPONENTS_OUT}/`)
-    console.log(`Saved: Combined folders ${time()}`)
+    successReporter(`Saved: Combined folders ${time()}`)
   }
 }
 
@@ -173,7 +173,7 @@ function buildStyles() {
       .sort(a => a.indexOf('@charset') === 0 ? -1 : 0)
       .join('\n')
     fs.writeFileSync(STYLE_FILE_OUT, compiledImportAtTop, 'utf-8')
-    console.log(`Saved: Component styles ${time()}`)
+    successReporter(`Saved: Component styles ${time()}`)
   }
 }
 
@@ -186,7 +186,7 @@ function buildStylesEditor() {
 
     const compiled = sass.compileString(editorcss, { loadPaths: [PATH] })
     fs.writeFileSync(STYLE_EDITOR_FILE_OUT, compiled.css, 'utf-8')
-    console.log(`Saved: Editor styles ${time()}`)
+    successReporter(`Saved: Editor styles ${time()}`)
   }
 }
 
@@ -194,7 +194,7 @@ function buildStylesAdmin() {
   if (config.styles_admin) {
     const compiled = sass.compile(STYLE_ADMIN_FILE_IN, { loadPaths: [PATH] })
     fs.writeFileSync(STYLE_ADMIN_FILE_OUT, compiled.css, 'utf-8')
-    console.log(`Saved: Admin styles ${time()}`)
+    successReporter(`Saved: Admin styles ${time()}`)
   }
 }
 
@@ -207,7 +207,7 @@ function buildStylesLayout() {
 
     const compiled = sass.compileString(layoutcss, { loadPaths: [PATH] })
     fs.writeFileSync(STYLE_LAYOUT_FILE_OUT, compiled.css, 'utf-8')
-    console.log(`Saved: Layout styles ${time()}`)
+    successReporter(`Saved: Layout styles ${time()}`)
   }
 }
 
@@ -215,7 +215,7 @@ function buildStylesVariables() {
   if (config.styles_variables) {
     const compiled = sass.compile(STYLE_VARIABLE_FILE_IN, { loadPaths: [COMPONENT_DIR] })
     fs.writeFileSync(STYLE_VARIABLE_FILE_OUT, compiled.css, 'utf-8')
-    console.log(`Saved: Variable styles ${time()}`)
+    successReporter(`Saved: Variable styles ${time()}`)
   }
 }
 
@@ -228,7 +228,7 @@ function buildStylesStories() {
 
     const compiled = sass.compileString(storybookcss, { loadPaths: [COMPONENT_DIR, PATH] })
     fs.writeFileSync(STYLE_STORIES_FILE_OUT, compiled.css, 'utf-8')
-    console.log(`Saved: Stories styles ${time()}`)
+    successReporter(`Saved: Stories styles ${time()}`)
   }
 }
 
@@ -265,7 +265,7 @@ function buildJavascript() {
           return `Drupal.behaviors.${i.name} = {attach: function (context, settings) {\n${i.body}\n}};`
         })
       ].join('\n'), 'utf-8')
-      console.log(`Saved: Compiled javascript (drupal) ${time()}`)
+      successReporter(`Saved: Compiled javascript (drupal) ${time()}`)
     }
 
     // Write JS file with dom content loaded wrapper.
@@ -276,7 +276,7 @@ function buildJavascript() {
           return `document.addEventListener('DOMContentLoaded', () => {\n${i.body}\n});`
         })
       ].join('\n'), 'utf-8')
-      console.log(`Saved: Compiled javascript (storybook) ${time()}`)
+      successReporter(`Saved: Compiled javascript (storybook) ${time()}`)
     }
   }
 }
@@ -284,7 +284,7 @@ function buildJavascript() {
 function buildAssetsDirectory() {
   if (config.assets) {
     runCommand(`rsync -a --delete --prune-empty-dirs --exclude .gitkeep --exclude js --exclude sass ${DIR_ASSETS_IN}/ ${DIR_ASSETS_OUT}/`)
-    console.log(`Saved: Assets ${time()}`)
+    successReporter(`Saved: Assets ${time()}`)
   }
 }
 
@@ -302,7 +302,7 @@ async function buildConstants() {
       SCSS_VARIABLES: scssVariableImporter.getVariables(),
     }
     fs.writeFileSync(CONSTANTS_FILE_OUT, JSON.stringify(constants, null, 2), 'utf-8')
-    console.log(`Saved: Compiled constants ${time()}`)
+    successReporter(`Saved: Compiled constants ${time()}`)
   }
 }
 
@@ -311,18 +311,21 @@ async function buildConstants() {
 async function build() {
   startTime = new Date().getTime()
   lastTime = startTime
-
-  buildOutDirectory()
-  buildCombineDirectories()
-  buildStyles()
-  buildStylesEditor()
-  buildStylesAdmin()
-  buildStylesLayout()
-  buildStylesVariables()
-  buildStylesStories()
-  buildJavascript()
-  buildAssetsDirectory()
-  await buildConstants()
+  try {
+    buildOutDirectory()
+    buildCombineDirectories()
+    buildStyles()
+    buildStylesEditor()
+    buildStylesAdmin()
+    buildStylesLayout()
+    buildStylesVariables()
+    buildStylesStories()
+    buildJavascript()
+    buildAssetsDirectory()
+    await buildConstants()
+  } catch (error) {
+    errorReporter(error);
+  }
 
   console.log(`Time taken: ${time(true)}`)
 }
@@ -413,4 +416,13 @@ function time(full) {
   const rtn = now - (full ? startTime : lastTime)
   lastTime = now
   return `[ ${rtn} ms ]`
+}
+
+function errorReporter(error) {
+  console.error('❌   Error during SASS compilation:', error.message);
+  console.error('Details:', error.formatted || error);
+}
+
+function successReporter(message) {
+  console.log(`✅   ${message}`)
 }
