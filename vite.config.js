f738c5b335576481b979e0fd8c29e6224b1044ec
Alan Cole <alan.cole@salsa.digital>
2025-03-25 17:30:09 +1100

    [#509] Revert changes to vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,11 @@ export default defineConfig(({ mode }) => ({
   plugins: [
     twig({
       namespaces: {
-        civictheme: resolve(import.meta.dirname, './components'),
+        'base': resolve(import.meta.dirname, './components/00-base'),
+        'atoms': resolve(import.meta.dirname, './components/01-atoms'),
+        'molecules': resolve(import.meta.dirname, './components/02-molecules'),
+        'organisms': resolve(import.meta.dirname, './components/03-organisms'),
+        'templates': resolve(import.meta.dirname, './components/04-templates'),
       },
     }),
     // This plugin allow watching files in the ./dist folder.
