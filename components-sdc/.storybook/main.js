b0b525146a6419d56bc257bb4422760ace18764f
Alan Cole <alan.cole@salsa.digital>
2025-03-25 17:26:16 +1100

    [#509] Updated storybook customization into a single plugin file.

--- components-sdc/.storybook/main.js
+++ components-sdc/.storybook/main.js
@@ -1,7 +1,4 @@
-import dependencies from './ct-dependencies'; // eslint-disable-line import/no-unresolved
 import path from 'path'
-
-const componentPath = path.resolve(__dirname, '../components');
 
 const config = {
   stories: [
@@ -17,39 +14,5 @@ const config = {
     options: {},
   },
   staticDirs: [{ from: '../dist/assets', to: '/assets' }, './static'],
-  viteFinal: (config) => {
-    // Add new imports to the stories.js files
-    config.plugins.push({
-      name: 'sdc-stories',
-      enforce: 'pre',
-      transform: (code, id) => {
-        if (id.endsWith('.stories.js')) {
-          // Find and import all dependencies from a story (and it's referenced twig files)
-          const loadedDependencies = dependencies.getDeps(id).map(i => `import '${i}';`).join('\n');
-          return {
-            code: `${loadedDependencies}\n${code}`,
-            map: null,
-          };
-        }
-        return null;
-      },
-    });
-
-    config.plugins.push({
-      name: 'sdc-js-wrapper',
-      transform: (code, id) => {
-        // Only process js files in the component directory that are not stories.
-        if (id.indexOf(componentPath) >= 0 && id.endsWith('.js') && !id.endsWith('stories.js') && !id.endsWith('stories.data.js')) {
-          return {
-            code: `document.addEventListener('DOMContentLoaded', () => {\n${code}\n});`,
-            map: null
-          };
-        }
-        return null;
-      }
-    })
-
-    return config;
-  },
 }
 export default config;
--- components-sdc/vite.config.js
+++ components-sdc/vite.config.js
@@ -1,14 +1,18 @@
 import { defineConfig } from 'vite';
 import { resolve } from 'path';
 import twig from 'vite-plugin-twig-drupal';
+import sdcPlugin from './.storybook/sdc-plugin.js';
+
+const componentDirectory = resolve(import.meta.dirname, './components');
 
 export default defineConfig(({ mode }) => ({
   plugins: [
     twig({
       namespaces: {
-        civictheme: resolve(import.meta.dirname, './components'),
+        civictheme: componentDirectory,
       },
     }),
+    sdcPlugin({ path: componentDirectory }),
     // This plugin allow watching files in the ./dist folder.
     {
       name: 'watch-dist',
