5479f88feb9413381af074bcc58e9dfe7c49eb03
Alan <alan.cole@salsa.digital>
2025-04-04 11:03:15 +1100

    [#509] Added SDC components (#512)

---
+++ components-sdc/scripts/install.sh
@@ -0,0 +1,17 @@
+rsync -a --delete ../../components/ ../components/
+
+echo "-> Includes before:"
+grep --include=*.twig -rnw ../components -e "@\(base\|atoms\|molecules\|organisms\)/.*/.*\.twig" | wc -l | awk '{print $1}'
+
+echo "-> Replace twig includes"
+node replace.mjs
+
+echo "-> Includes after:"
+grep --include=*.twig -rnw ../components -e "@\(base\|atoms\|molecules\|organisms\)/.*/.*\.twig" | wc -l | awk '{print $1}'
+
+# ----
+
+echo "-> Build"
+cd ../
+npm run dist
+cd scripts
---
+++ components-sdc/vite.config.js
@@ -0,0 +1,31 @@
+import { defineConfig } from 'vite';
+import { resolve } from 'path';
+import twig from 'vite-plugin-twig-drupal';
+import sdcPlugin from './.storybook/sdc-plugin.js';
+
+const componentDir = resolve(import.meta.dirname, './components');
+
+export default defineConfig(({ mode }) => ({
+  plugins: [
+    twig({
+      namespaces: {
+        civictheme: componentDir,
+      },
+    }),
+    sdcPlugin({ path: componentDir }),
+    // This plugin allow watching files in the ./dist folder.
+    {
+      name: 'watch-dist',
+      configureServer: (server) => {
+        server.watcher.options = {
+          ...server.watcher.options,
+          ignored: [
+            '**/.git/**',
+            '**/node_modules/**',
+            '**/.logs/**',
+          ]
+        }
+      }
+    }
+  ],
+}));
