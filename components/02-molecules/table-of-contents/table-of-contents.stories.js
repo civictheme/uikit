9bb860a687276fc6ea429f63da0cf9dbc5925be3
Alan Cole <alan.cole@salsa.digital>
2025-04-16 17:01:47 +1000

    Updated TOC - Moved auto / manual logic into .twig. Removed story twig.

--- components/02-molecules/table-of-contents/table-of-contents.stories.js
+++ components/02-molecules/table-of-contents/table-of-contents.stories.js
@@ -1,4 +1,5 @@
-import Component from './table-of-contents.stories.twig';
+import Component from './table-of-contents.twig';
+import BasicContent from '../basic-content/basic-content.twig';
 
 const meta = {
   title: 'Molecules/Table Of Contents',
@@ -9,6 +10,12 @@ const meta = {
       options: ['light', 'dark'],
     },
     title: {
+      control: { type: 'text' },
+    },
+    anchor_selector: {
+      control: { type: 'text' },
+    },
+    scope_selector: {
       control: { type: 'text' },
     },
     position: {
@@ -60,6 +67,19 @@ export const TableOfContentsAutomatic = {
     anchor_selector: 'h2',
     scope_selector: '.ct-basic-content',
     position: 'before',
+    content: BasicContent({ content: `
+      <h2>Heading 2 - 1</h2>
+      <p>Ex cillum irure id occaecat aliquip in cillum aute occaecat ullamco in dolore nulla et veniam sed consectetur ut excepteur eu eiusmod excepteur nulla id mollit veniam deserunt ea nostrud.</p>
+      <h3>Heading 3</h3>
+      <h4>Heading 4</h4>
+      <h5>Heading 5</h5>
+      <h6>Heading 6</h6>
+      <p>Consectetur veniam exercitation voluptate reprehenderit in esse est magna minim sunt cupidatat reprehenderit ut pariatur cillum do aute adipisicing commodo voluptate quis in tempor eu irure velit esse non dolore officia sit cupidatat officia fugiat mollit eu.</p>
+      <h2>Heading 2 - 2</h2>
+      <p>Est incididunt irure eu elit eiusmod incididunt occaecat labore aute in ad non irure sunt ad ut nostrud commodo do fugiat fugiat tempor occaecat mollit sunt in id sed commodo enim occaecat eu proident nostrud fugiat cupidatat.</p>
+      <h2>Heading 2 - 3</h2>
+      <p>Nulla sed cupidatat irure quis veniam ut in in pariatur do minim adipisicing minim exercitation magna eiusmod culpa tempor.</p>
+    ` }),
     modifier_class: '',
     attributes: '',
   },
