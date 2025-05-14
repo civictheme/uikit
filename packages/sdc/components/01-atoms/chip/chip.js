7e806c91bbb8da2624ec1ae164a2e213aa11995a
Alan Cole <alan.cole@salsa.digital>
2025-05-14 19:56:18 +1000

    [#497] Chip should only set events if chip is an input.

--- packages/sdc/components/01-atoms/chip/chip.js
+++ packages/sdc/components/01-atoms/chip/chip.js
@@ -109,6 +109,6 @@ CivicThemeChip.prototype.findChip = function (el) {
   return el.closest('.ct-chip');
 };
 
-document.querySelectorAll('.ct-chip').forEach((el) => {
+document.querySelectorAll('[data-component-name="chip"]').forEach((el) => {
   new CivicThemeChip(el);
 });
